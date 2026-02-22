51ac319a7c862fd1f55e2fa63dfd420f6d669c72
Khaled Sameh <eng.khaled.sameh.360@gmail.com>
2026-02-22 21:52:18 +0200

    create jot view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:jot_app/view/jot_view.dart';
 
 void main() {
-  
+  runApp(JotApp());
 }
-
+class JotApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: JotView(),
+    );
+  }
+}
