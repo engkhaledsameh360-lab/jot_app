6dbe1cf494d58121ae04f15e2aaf4237b1824dad
Khaled Sameh <eng.khaled.sameh.360@gmail.com>
2026-02-24 20:22:14 +0200

    Create Jot models

---
+++ lib/models/jot_models.dart
@@ -0,0 +1,13 @@
+class JotModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  JotModel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.color,
+  });
+}
